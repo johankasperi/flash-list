c8287054ec90a2059dc44f483dbbd1e6053724b3
Daniel Friyia JR <daniel.friyia@shopify.com>
2023-02-10 09:17:15 -0500

    [DF] feat(#547): `experimentalMaintainTopContentPosition` works vertically

--- src/FlashList.tsx
+++ src/FlashList.tsx
@@ -163,6 +163,7 @@ class FlashList<T> extends React.PureComponent<
     prevState: FlashListState<T>
   ): FlashListState<T> {
     const newState = { ...prevState };
+
     if (prevState.numColumns !== nextProps.numColumns) {
       newState.numColumns = nextProps.numColumns || 1;
       newState.layoutProvider = FlashList.getLayoutProvider<T>(
